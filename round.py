924e19e68686a28673ebfe9bf5cf71ce5cca9dd1
Marcus-Mosley <68971666+Marcus-Mosley@users.noreply.github.com>
2020-11-13 21:19:00 -0500

    Added in round.py

--- round.py
+++ round.py
@@ -2,13 +2,13 @@
 
 # Created by Marcus A. Mosley
 # Created on November 2020
-# This program gives the proper format for a Canadian Address
+# This program calculates a rounded number
 
 import math
 
 
 def round_number(num, precision):
-    # Gets a users name and prints out their formal name
+    # Rounds a number to a specified precision
 
     num[0] = num[0] * 10**precision[0] + 0.5
     num[0] = int(num[0])
@@ -21,6 +21,7 @@ def main():
     number_var = []
     round_precision_var = []
 
+    # Input
     number = float(input("Enter the number you want to round: "))
     round_precision = float(input("Enter how many decimal places"
                                   " you want to round by: "))
@@ -28,6 +29,7 @@ def main():
     number_var.append(number)
     round_precision_var.append(round_precision)
 
+    # Call Functions
     round_number(number_var, round_precision_var)
 
     print("The rounded number is {0}".format(number_var[0]))
